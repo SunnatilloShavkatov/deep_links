704f400b8642b6c4687448bc4ebc7cf509fe73b5
Sunnatillo Shavkatov <sunnatilloshavkatov@gmail.com>
2022-01-02 16:57:35 +0500

    ios release done

--- lib/core/constants/app_theme.dart
+++ lib/core/constants/app_theme.dart
@@ -6,9 +6,7 @@ import 'colors.dart';
 final ThemeData appThemeData = ThemeData(
   visualDensity: VisualDensity.adaptivePlatformDensity,
   primaryColor: Colors.white,
-  accentColor: clrAssets,
   appBarTheme: AppBarTheme(
-    brightness: Brightness.light,
     color: Colors.white,
     elevation: 1,
     systemOverlayStyle: SystemUiOverlayStyle(
@@ -19,6 +17,7 @@ final ThemeData appThemeData = ThemeData(
       systemNavigationBarIconBrightness: Brightness.light,
       systemNavigationBarDividerColor: Colors.grey,
     ),
+    titleTextStyle: TextStyle(color: Colors.black)
   ),
   scaffoldBackgroundColor: clrBackground,
   fontFamily: 'SFPro',
